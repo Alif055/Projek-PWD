076783f782be8cea602f662750401a4e5a7773ce
digheag <digheagumaisa@gmail.com>
2025-05-29 08:55:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ include 'koneksi.php';
   <li class="nav-item"><a class="nav-link" href="keranjang.php">Keranjang</a></li>
 
   <?php if (isset($_SESSION['login'])): ?>
-    <li class="nav-item"><a class="nav-link" href="#">Halo, <?= $_SESSION['user']; ?></a></li>
     <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
   <?php else: ?>
     <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
